b37605fd3ff383813f11617b88504c73609b4949
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-04-06 18:41:58 -0300

    feat(pymeet-rest): add get users endpoint

--- pymeet-rest/src/pymeet/entrypoints/v1/user.py
+++ pymeet-rest/src/pymeet/entrypoints/v1/user.py
@@ -5,10 +5,10 @@ This module contains the entry point for the user domain object.
 from typing import Annotated
 
 from fastapi import APIRouter, HTTPException, Depends
-from starlette.status import HTTP_409_CONFLICT, HTTP_201_CREATED
+from starlette.status import HTTP_409_CONFLICT, HTTP_201_CREATED, HTTP_200_OK
 
 from pymeet.domain.schemas import UserIn, UserResponse, BaseUser
-from pymeet.services.dependencies import get_register_service
+from pymeet.services.dependencies import get_register_service, UserRepositoryDependency
 from pymeet.services.register import IllegalUserException, RegisterService
 
 router: APIRouter = APIRouter(prefix="/users", tags=["users"])
@@ -30,3 +30,14 @@ def register_user(user_form: UserIn, user_repository: RegisterServiceDependency)
         raise HTTPException(status_code=HTTP_409_CONFLICT, detail=str(e)) from e
 
     return UserResponse(data=BaseUser(**{"username": user.username, "email": user.email}))
+
+
+@router.get("/", status_code=HTTP_200_OK)
+def get_users(user_repository: UserRepositoryDependency) -> UserResponse:
+    """
+    Get all users.
+    """
+
+    users = list((BaseUser(**{"username": user.username, "email": user.email}) for user in user_repository.find_all()))
+
+    return UserResponse(data=users)
