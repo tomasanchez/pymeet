3d5a6868b9e7cf2832902cd234dc0b25de727c57
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-04-06 17:11:23 -0300

    feat(pymeet-rest): add v1.users entrypoint

--- pymeet-rest/src/pymeet/app/router.py
+++ pymeet-rest/src/pymeet/app/router.py
@@ -8,11 +8,15 @@ Resources:
 from fastapi import APIRouter
 
 from pymeet.entrypoints import base
+from pymeet.entrypoints.v1 import user
 
-root_api_router_v1 = APIRouter(prefix="/api/v1")
+root_api_router_v1 = APIRouter(prefix="/api/v1", tags=["v1"])
 base_router = APIRouter()
 
 # Base Routers
 base_router.include_router(base.router)
 
 # API Routers
+
+# V1
+root_api_router_v1.include_router(user.router)
